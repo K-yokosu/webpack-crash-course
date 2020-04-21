dbb7c6112e47ca0ed2d7df0a3265fcf97866469d
K-yokosu <kkk_ns_ili-1125@ezweb.ne.jp>
2020-04-21 03:01:46 +0000

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import _ from 'lodash'
 
+//importのいろいろ
+import { NiJou, NAME } from './utilities'
+import * as utilities from './utilities'
+import { NAME as NAME_OF_HAM } from './utilities'
+import Lion from './utilities'
+import Tiger from './utilities'
+
+console.log(NiJou(3));
+console.log(NAME);
+console.log(utilities.NAME);
+console.log(NAME_OF_HAM);
+console.log(Lion.say());
+console.log(Tiger.say());
 
 function component(){
     const element = document.createElement('div');
